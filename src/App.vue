8a471712ff10a1f9cec0e171c806cf39ce1d10e0
ryanC1993 <tommy149347@gmail.com>
2017-06-09 01:19:53 +0800

    + scroll effect

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
-  #app
+  #app( @scroll="handleScroll" )
     Landing
     Search
+    a(id="toS" href="#search" v-smooth-scroll="{ duration: 1000 }" )
+    a(id="toL" href="#landing" v-smooth-scroll="{ duration: 1000 }")
     //- router-view
 </template>
 
@@ -12,6 +14,42 @@ import Search from '@/components/Search.vue'
 
 export default {
   name: 'app',
+  data () {
+    return {
+      scrollPosition: 0,
+      isResearch: false,
+    }
+  },
+  methods: {
+    handleScroll: function(e) {
+      if (e.deltaY < 0) {
+        this.clickToLanding();
+      }
+      else if (e.deltaY > 0) {
+        this.clickToSearch();
+      }
+    },
+    clickToSearch: function() {
+      if (this.isResearch === false){
+        console.log('scrolling down');
+        document.getElementById("toS").click();
+        this.isResearch = true;
+      }
+    },
+    clickToLanding: function() {
+      if (this.isResearch === true){
+        console.log('scrolling up');
+        document.getElementById("toL").click();
+        this.isResearch = false;
+      }
+    }
+  },
+  created () {
+    window.addEventListener('wheel', this.handleScroll);
+  },
+  destroyed () {
+    window.removeEventListener('wheel', this.handleScroll);
+  },
   components: {
     Landing,
     Search
