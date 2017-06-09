96eb642acf07823f5ee063458736cee81d0dc636
吳育騫 <andrew90622@gmail.com>
2017-06-09 13:51:00 +0800

    search bar

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,11 @@
 <template lang="pug">
   #search
+    .content
+      .search-holder
+        form
+        .awesomplete
+         input(placeholder="Search Domain" v-model="searchString")
+         .button
 </template>
 
 <script>
@@ -23,4 +29,41 @@ export default {
   +setSize
   background-color: $secondary-color
 
+.content
+  width: 100%
+  height: 50%
+  padding: 2em 5em
+  box-sizing: border-box
+  max-width: 64em
+  margin: auto
+  
+.search-holder
+  +center
+  width: 100%
+  height: 50%
+  position: relative
+
+.awesomplete
+  width: 100%
+  height: 100%
+
+
+.awesomplete input
+  +center
+  border-radius: 10px
+  display: block
+  padding: 0.8em 3em 0.8em 0.8em
+  font-size: 1.4em
+  border: none
+  &:focus
+    outline: none
+
+.button
+  +center
+  left: 65%
+  width: 30px
+  height: 30px
+  background-image: url('../assets/search.svg')
+  background-repeat: no-repeat
+  font-size: 2em
 </style>
