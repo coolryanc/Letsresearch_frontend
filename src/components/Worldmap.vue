7db3d999e3e33c54e804c08816dd71fefc165249
ryanC1993 <tommy149347@gmail.com>
2017-06-12 18:13:15 +0800

    + mapVue

---
+++ src/components/Worldmap.vue
@@ -0,0 +1,55 @@
+<template lang="pug">
+  #worldmap
+</template>
+
+<script>
+
+// import * as d3 from "d3"
+// import * as topojson from "topojson"
+
+export default {
+  name: 'worldmap',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted() {
+    var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    mapboxgl.accessToken = 'pk.eyJ1IjoieXVhbm5uIiwiYSI6ImNqM3R2dDZuYzAxbjQycG82NjExaWtiaG0ifQ.HY_IKgp-eD6ER5J7rJFKBQ';
+    var map = new mapboxgl.Map({
+      container: 'worldmap',
+      style: 'mapbox://styles/yuannn/cj3twn3b800232rnhsgboa8ix'
+    });
+
+  },
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+
+@import "../sass/variables"
+@import "../sass/common"
+
+#worldmap
+  width: 70%
+  height: 100%
+  // background-color: white
+  box-shadow: 0 10px 10px 2px rgba(black, 0.5)
+  overflow: hidden
+
+svg
+  // background-color: #222
+  &:active
+    cursor: grab
+  &:hover
+  .country
+    stroke: white
+    stroke-width: 10px
+
+
+
+
+</style>
