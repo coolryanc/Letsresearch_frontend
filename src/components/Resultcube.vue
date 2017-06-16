2e907e5f42bf205052500bc3c0844a7007d9ebe7
ryanC1993 <tommy149347@gmail.com>
2017-06-16 08:43:52 +0800

    + result component

---
+++ src/components/Resultcube.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  .resultCube
+    .schoolTile {{schoolData.schoolName}}
+</template>
+
+<script>
+
+// import * as d3 from "d3"
+// import * as topojson from "topojson"
+
+export default {
+  name: 'resultcube',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  },
+  props: {
+    schoolData: Object
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass">
+
+@import "../sass/variables"
+@import "../sass/common"
+
+.resultCube
+  width: 100%
+  height: 200px
+  background-color: #999
+  border-bottom: solid 10px #222
+
+
+</style>
